9f9331c8442ecb98f773f7e29c0e9ca95a70e0ee
sushantk <561855+sushantk@users.noreply.github.com>
2024-02-15 15:31:11 -0800

    remove urn:uuid: from id.

--- js/model/base.js
+++ js/model/base.js
@@ -350,6 +350,7 @@ define
                     UNIQUE_NUMBER++;
                     ret.id = "element_" + UNIQUE_NUMBER;
                 }
+                ret.id = ret.id.replace("urn:uuid:", "");
                 let elements = context.parsed.Element;
                 if (null == elements)
                     context.parsed.Element = elements = {};
